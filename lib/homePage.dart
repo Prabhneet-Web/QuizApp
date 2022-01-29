a6e9c4b57db4d3f614662550858fa9c003378d2d
Prabhneet Narula <imailcode@gmail.com>
2022-01-29 22:40:07 +0530

    HomePage Added.

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,2 +1,29 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("lib/assets/images/Quiz2.webp"),
+              fit: BoxFit.cover)),
+      child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(begin: Alignment.bottomCenter, colors: [
+            Colors.black.withOpacity(0.8),
+            Colors.black.withOpacity(0.6),
+            Colors.black.withOpacity(0.2)
+          ])),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          )),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/homePage.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,6 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      
+      home: HomePage(),
+    );
   }
 }
