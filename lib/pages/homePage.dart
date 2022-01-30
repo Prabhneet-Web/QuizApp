bc9d549f94b59332896dd94af76db984bd404265
Prabhneet Narula <imailcode@gmail.com>
2022-01-30 13:15:19 +0530

    FirstPage Added!

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,8 +1,43 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:quiz_app/pages/firstPage.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 100));
+
+    _animation =
+        Tween<double>(begin: 1.0, end: 25.0).animate(_animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void onTap() {
+    _animationController.forward().then((f) => Navigator.push(
+        context,
+        PageTransition(
+            child: const FirstPage(),
+            type: PageTransitionType.rotate,
+            alignment: Alignment.bottomCenter)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +61,7 @@ class HomePage extends StatelessWidget {
               Row(
                 children: [
                   const SizedBox(
-                    width: 15,
+                    width: 12,
                     height: 170,
                   ),
                   const Text("Play To",
@@ -36,12 +71,12 @@ class HomePage extends StatelessWidget {
                           fontWeight: FontWeight.bold)),
                   const SizedBox(width: 15),
                   AnimatedTextKit(animatedTexts: [
-                    RotateAnimatedText("WIN",
+                    RotateAnimatedText("WIN!",
                         textStyle: const TextStyle(
                             fontSize: 50,
                             color: Colors.white,
                             fontFamily: "Horizon")),
-                    RotateAnimatedText("LOSE",
+                    RotateAnimatedText("LOSE!",
                         textStyle: const TextStyle(
                             fontSize: 50,
                             color: Colors.white,
@@ -49,20 +84,23 @@ class HomePage extends StatelessWidget {
                   ], isRepeatingAnimation: true, totalRepeatCount: 30)
                 ],
               ),
-              Container(
-                padding: EdgeInsets.only(left: 8, right: 8),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    gradient: const LinearGradient(colors: [
-                      Color.fromARGB(255, 200, 152, 67),
-                      Color.fromARGB(255, 255, 219, 143)
-                    ])),
-                child: MaterialButton(
-                  onPressed: () {},
-                  minWidth: double.infinity,
-                  child: const Text(
-                    "Start",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, bottom: 15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      gradient: const LinearGradient(colors: [
+                        Color.fromARGB(255, 200, 152, 67),
+                        Color.fromARGB(255, 255, 219, 143)
+                      ])),
+                  child: MaterialButton(
+                    onPressed: () => onTap(),
+                    minWidth: double.infinity,
+                    child: const Text(
+                      "Start",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
                   ),
                 ),
               ),
